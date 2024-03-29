7a91e73951a680de1121d7f8e2647d7ea29c13b8
Parikshit <parikshit9112@gmail.com>
2024-03-29 06:21:07 +0530

    useFetch,router,link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import Create from './Create';
 import Home from './Home';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
      <div className="content">
-       <Home />
+      <Switch>
+        <Route  exact path="/">
+          <Home />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,15 @@
-import { useState ,useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
- 
  
  const Home = () => {
-     const [blogs,setBlogs]=useState(null);
-     const [isPending,setPending]=useState(true);
-     const [error,setError]=useState(null);
-   
-
-     const handleDelete=(id)=>{
-      const newBlog=blogs.filter( blog => blog.id !==id );
-      setBlogs(newBlog);
-     }
-      
-     useEffect(() => {
-     setTimeout(()=>{
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if(!res.ok){
-         throw Error('Error 404 NOT FOUND');
-        }
-         return res.json();
-      })
-      .then (data =>{
-         setBlogs(data);
-         setPending(false);
-         
-      })
-      .catch(err =>{
-         setError(err.message);
-         setPending(false);
-      })
-     },1000);
-     },[]);
+    const {data :blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
 
     return ( 
       <div className="home">
          {error && <div>{error}</div>}
          {isPending && <div>Loading.... </div>}
-        {blogs &&<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+        {blogs &&<BlogList blogs={blogs} title="All Blogs"  />}
         
        
       </div>
